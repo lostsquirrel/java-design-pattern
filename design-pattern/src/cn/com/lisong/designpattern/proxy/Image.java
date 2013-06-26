ad558707944cd686a2e5f27ca85ae2dcb930d76a
lisong <songli1988@gmail.com>
2013-06-26 10:41:07 +0800

    add Proxy Design Pattern

---
+++ design-pattern/src/cn/com/lisong/designpattern/proxy/Image.java
@@ -0,0 +1,5 @@
+package cn.com.lisong.designpattern.proxy;
+
+public interface Image {
+	void display();
+}
---
+++ design-pattern/src/cn/com/lisong/designpattern/proxy/TestProxy.java
@@ -0,0 +1,17 @@
+package cn.com.lisong.designpattern.proxy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestProxy {
+
+	@Test
+	public void testName() throws Exception {
+		Image image = new ProxyImage("test_10mb.jpg");
+		//image will be loaded from disk
+		image.display();
+		//image will not be loaded from disk
+		image.display(); 
+	}
+}
